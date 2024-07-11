1d6b985353534cdf5dfa602bd06cb77cd3fc029b
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-11 16:04:28 -0300

    updating the domain

--- src/main/java/br/com/desafioapiplaces/domain/Place.java
+++ src/main/java/br/com/desafioapiplaces/domain/Place.java
@@ -1,7 +1,14 @@
 package br.com.desafioapiplaces.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
-public record Place (Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt){
-
+public record Place(
+        @Id Long id, String name, String slug,
+         String city, String state,
+        @CreatedDate LocalDateTime createdAt,
+        @LastModifiedDate LocalDateTime updatedAt) {
 }
--- src/main/java/br/com/desafioapiplaces/domain/PlaceService.java
+++ src/main/java/br/com/desafioapiplaces/domain/PlaceService.java
@@ -1,19 +1,23 @@
 package br.com.desafioapiplaces.domain;
 
+import br.com.desafioapiplaces.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
-    public Mono<Place> create(Place place){
+    public Mono<Place> create(PlaceRequest placeRequest){
+        var place = new Place(null, placeRequest.name(),slg.slugify(placeRequest.name()),
+                placeRequest.city(), placeRequest.state(), null,null);
        return placeRepository.save(place);
     }
-
-
 
 
 }
