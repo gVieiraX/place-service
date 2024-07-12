586139f021753389cd85f27c0302285b8d1641c4
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-11 23:46:29 -0300

    adding the endpoints GET,PUT and DELETE

--- src/main/java/br/com/desafioapiplaces/controller/PlaceController.java
+++ src/main/java/br/com/desafioapiplaces/controller/PlaceController.java
@@ -8,11 +8,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/places")
@@ -25,5 +26,25 @@ public class PlaceController {
     public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request) {
         var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Flux<Place>> getAll(){
+        Flux<Place> placeResponseFlux = this.placeService.getAll();
+        return ResponseEntity.ok().body(placeResponseFlux);
+
+    }
+
+    @PutMapping
+    public Mono<Place> update(@RequestBody Place place){
+        return placeService.update(place);
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<ResponseEntity<String>> delete(@PathVariable("id") Long id) {
+        return placeService.delete(id)
+                .then(Mono.just(ResponseEntity.ok("Deletado com sucesso")))
+                .onErrorResume(e -> Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage())));
     }
 }
